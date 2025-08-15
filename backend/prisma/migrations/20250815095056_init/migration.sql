a8dbb7340d54807684d6096f804bae4337f26b59
Janith-Chamikara <janithchamikara13@gmail.com>
2025-08-15 16:59:21 +0530

    update: onboarding request body

---
+++ backend/prisma/migrations/20250815095056_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "divisionalSecretariat" TEXT;
+ALTER TABLE "User" ADD COLUMN "gnDivision" TEXT;
--- backend/src/auth/dto/sign-up.dto.ts
+++ backend/src/auth/dto/sign-up.dto.ts
@@ -1,55 +1,13 @@
-import {
-  IsDefined,
-  IsNotEmpty,
-  IsEmail,
-  MinLength,
-  IsString,
-  IsOptional,
-} from 'class-validator';
+import { IsDefined, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
 
 export class SignUpDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly firstName: string;
-
-  @IsString()
-  @IsOptional()
-  readonly lastName: string;
-
-  @IsString()
-  @IsNotEmpty()
-  readonly phoneNumber: string;
-
-  @IsString()
-  @IsNotEmpty()
-  readonly nationalId: string;
-
-  @IsString()
-  @IsNotEmpty()
-  readonly dateOfBirth: string;
-
   @IsDefined()
+  @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
   @IsDefined()
   @IsNotEmpty()
-  @MinLength(8)
+  @MinLength(6)
   readonly password: string;
-
-  @IsString()
-  @IsOptional()
-  readonly address: string;
-
-  @IsString()
-  @IsOptional()
-  readonly city: string;
-
-  @IsString()
-  @IsOptional()
-  readonly province: string;
-
-  @IsString()
-  @IsOptional()
-  readonly postalCode: string;
 }
